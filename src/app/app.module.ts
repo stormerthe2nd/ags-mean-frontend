43e5d19a60c263f65b21c33a97ab98ec98bca63d
Junaid Firdosi <theiconsting17@gmail.com>
2021-07-08 02:11:53 +0530

    post-create component moved to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component'
-import { postCreate } from './dashboard/post-create/post-create.component';
 import { AboutComponent } from './about/about.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    postCreate,
     AboutComponent
   ],
   imports: [
