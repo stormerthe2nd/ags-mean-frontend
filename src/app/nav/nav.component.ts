67179b383f0ebc62be3fa797810e519316467552
Junaid Firdosi <theiconsting17@gmail.com>
2021-07-11 04:27:56 +0530

    alerts added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../posts';
 
 @Component({
@@ -11,18 +12,25 @@ export class NavComponent {
   @Input() today: string;
   constructor() {
   }
-
-  addPost(desInp: HTMLTextAreaElement, fileInp: HTMLInputElement): void {
-    let imgPaths: Array<string> = []
-    imgPaths.push(`${fileInp.value}`)
+  postCreationStatus = "uninit"
+  addPost(form: NgForm): void {
+    this.postCreationStatus = "uninit"
+    if (form.value.desInp == "") {
+      this.postCreationStatus = "empty input"
+      document.getElementById("des").focus()
+      return
+    }
+    let imgPaths: Array<string> = ["https://source.unsplash.com/500x500/?nature"]
+    imgPaths.push(`${form.value.fileInp}`)
     this.postsArr.push({
       sno: this.postsArr.length + 1,
       imgPath: imgPaths,
-      des: desInp.value,
+      des: form.value.desInp,
       updated: this.today,
       active: true,
       category: ''
     })
+    this.postCreationStatus = "success"
     console.log(this.postsArr)
   }
 
