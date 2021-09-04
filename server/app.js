9a16089d574f2be79df55bc92c05fca747dd7b2c
Junaid Firdosi <theiconsting17@gmail.com>
2021-09-04 09:09:18 +0530

    nav ui changed

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept")
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS")
   next()
 })
 
