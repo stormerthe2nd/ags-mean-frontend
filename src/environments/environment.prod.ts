5f50bf9f5bda0157eae6ad8c8b0fd7633b951824
Junaid Firdosi <theiconsting17@gmail.com>
2021-11-04 10:20:25 +0530

    ready for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: "293019557085-bdtl828coo47kn5pqv4qi77rgj6c5rk0.apps.googleusercontent.com",
+  domain: "https://ags-market.herokuapp.com"
 };
